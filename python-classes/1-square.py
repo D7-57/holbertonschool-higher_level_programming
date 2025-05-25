6db3a07d8bbafda17b3794ca584d0c76264b2419
D7-57 <10665@holbertonstudents.com>
2025-05-25 16:47:46 +0000

    fix pycodestyle

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Defines an empty class Square."""
 
+
 class Square:
     """Represents a square."""
-    
-    def __init__(self,size):
+
+    def __init__(self, size):
         """Initializes the square with a private size.
 
         Args:
-            size (int): The size of the square."""
-        
-        self.__size=size
+            size (int): The size of the square.
+        """
+        self.__size = size
