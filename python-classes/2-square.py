6bfbc9ea6126eb1d13ef40668d5a48cbe12d0816
D7-57 <10665@holbertonstudents.com>
2025-05-25 08:42:56 +0000

    square raise error

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""Defines a class Square with a private size attribute."""
+"""This module defines a Square class with size validation."""
 
 class Square:
     """Represents a square."""
 
-    def __init__(self, size):
-        """Initializes the square with a private size.
-        
+    def __init__(self, size=0):
+        """Initializes the square with a validated private size.
+
         Args:
-            size (int): The size of the square.
+            size (int): The size of the square (default is 0).
 
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
-        """check if size is greater than 0 else throw an exception """
-
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        
         if size < 0:
             raise ValueError("size must be >= 0")
-        
         self.__size = size
