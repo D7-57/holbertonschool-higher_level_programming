0f872eb50b693c422b6932d4b7a34830190b68c7
D7-57 <10665@holbertonstudents.com>
2025-06-02 08:43:46 +0000

    function thats reads the content of a file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""a function thats allow us to choose a file path and print the content"""
+
+
 def read_file(filename=""):
+    """function take one argument the file path and print the content """
     with open(filename) as f:
         print(f.read())
