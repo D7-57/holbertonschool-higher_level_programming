c5bb89a090878d4bffd8825cec1ca44d9fe83b0b
D7-57 <10665@holbertonstudents.com>
2025-06-03 07:31:42 +0000

    save obj as JSON

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """a function that saves the JSON equivelannce to a pytohn obj on file"""
     with open(filename, "w", encoding="utf-8") as f:
-        json.dump(my_obj,f)
+        json.dump(my_obj, f)
