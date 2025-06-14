8cf2ffd90a1035d89434c2b3f3f99635843a5555
D7-57 <10665@holbertonstudents.com>
2025-06-14 17:59:09 +0000

    add readme and testing Python's requests module

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import requests
+import csv
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    
+    print(f"Status Code: {response.status_code}")
+    
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post["title"])
