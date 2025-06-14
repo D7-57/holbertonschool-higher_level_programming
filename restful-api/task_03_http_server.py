2aa11bf3abbd9f779b95abd3e43fb4a9cbe7c65d
D7-57 <10665@holbertonstudents.com>
2025-06-14 18:22:11 +0000

    Develop a simple API using Python with the  module

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python3
-import http.server
-import socketserver
-import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-PORT = 8000
-
-class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
+class SimpleAPIHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_header("Content-Type", "application/json")
-
         if self.path == "/":
             self.send_response(200)
             self.send_header("Content-Type", "text/plain")
@@ -27,13 +21,21 @@ class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b'{"status":"OK"}')
 
+        elif self.path == "/info":
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            self.wfile.write(b'{"version":"1.0","description":"A simple API built with http.server"}')
+
         else:
             self.send_response(404)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
             self.wfile.write(b'{"error":"Endpoint not found"}')
 
+
 if __name__ == "__main__":
-    with socketserver.TCPServer(("", PORT), SimpleAPIHandler) as httpd:
-        print(f"Serving on http://localhost:{PORT}")
-        httpd.serve_forever()
+    server_address = ("", 8000)
+    httpd = HTTPServer(server_address, SimpleAPIHandler)
+    print("Serving on http://localhost:8000")
+    httpd.serve_forever()
