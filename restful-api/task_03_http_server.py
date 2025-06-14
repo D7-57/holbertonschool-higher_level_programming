3c9dc901186c3efd885f56af51490db43c8d60cc
D7-57 <10665@holbertonstudents.com>
2025-06-14 18:25:14 +0000

    Develop a simple API using Python with the  module

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -29,9 +29,9 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
 
         else:
             self.send_response(404)
-            self.send_header("Content-Type", "application/json")
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'{"error":"Endpoint not found"}')
+            self.wfile.write(b"Endpoint not found")
 
 
 if __name__ == "__main__":
