dcf7a68bdf52bf5c87a03e3eabeea0ede860889d
D7-57 <10665@holbertonstudents.com>
2025-06-29 09:40:13 +0000

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states starting with 'N' from the database hbtn_0e_0_usa"""
+""" Lists all states from the database hbtn_0e_0_usa """
 
 import MySQLdb
 import sys
@@ -7,16 +7,11 @@ import sys
 if __name__ == "__main__":
     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=db_name)
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
     for row in cursor.fetchall():
         print(row)
