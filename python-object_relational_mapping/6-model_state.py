ba4f53a01964ad5a2be1953dcbebcab45fac4cdf
D7-57 <10665@holbertonstudents.com>
2025-06-29 11:41:50 +0000

    First state model

---
+++ python-object_relational_mapping/6-model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Create the states table in a given database using SQLAlchemy and the State model"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    # Create engine using provided MySQL credentials
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True
+    )
+    
+    # Create all tables defined via Base metadata (in this case, State)
+    Base.metadata.create_all(engine)
