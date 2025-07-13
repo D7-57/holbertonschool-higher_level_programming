fb4b0ed6bd75ac37e3633a8b1a686448948bcce5
D7-57 <10665@holbertonstudents.com>
2025-07-13 13:02:20 +0000

    loop to print

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const arg = process.argv[2];
-const number = parseInt(arg);
-if (!isNaN(number)) {
-  console.log('Missing number of occurrences');
-} else {
-  for (let i = 0; i < number; i++) {
-    console.log('C is fun');
+let number = parseInt(arg);
+if (isNaN(x)) {
+    console.log('Missing number of occurrences');
+  } else {
+    for (let i = 0; i < number; i++) {
+      console.log('C is fun');
+    }
   }
-}
